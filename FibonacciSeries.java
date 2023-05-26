6aa3beeed316b1d75b9a7e96ed14b1258ad98eba
ayushchauhands <112310498+ayushchauhands@users.noreply.github.com>
2023-05-26 19:51:52 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,10 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+       int n1=0, n2=1,sum=0;
+       for (int i=1;i<=20;i++){
+           sum = n1+n2;
+           System.out.print(" "+sum);
+           n1=n2;n2=sum;
+       }
+    }
+}
